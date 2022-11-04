672b52cea446a8b62b69515d7f46c85ae47167e4
Clara Lancelle <lancelle.clara@hotmail.com>
2022-11-04 21:25:08 +0100

    update data_base

--- data_base.php
+++ data_base.php
@@ -2,17 +2,10 @@
 
 function getConnexion(){
 
-<<<<<<< HEAD:data_base.php
     $servername = 'localhost';
     $dbname = 'recipes';
     $user = 'root';
     $password = '';
-=======
-    $servername = 'db';
-    $dbname = 'recipes';
-    $user = 'recipes';
-    $password = 'Nf6RQ1NKJ49nWZ7b0';
->>>>>>> 2b5d20c52d37e0d3f44100c1119faf8f34087355:includes/data_base.php
     $dbco = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8mb4",$user,$password);
 
     return $dbco;
